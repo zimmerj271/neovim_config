044bcb28b05d25cad2066d5f9852a1e0c072b1b3
Justin Zimmerman <justin.zimmerman@astranahealth.com>
2024-08-22 10:52:31 -0700

    Updated formatting

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
-require("settings") -- ./lua/settings.lua
-require("keymaps") -- ./lua/keymaps.lua
-require("autocommands") -- ./lua/autocommands.lua
+require("settings")      -- ./lua/settings.lua
+require("keymaps")       -- ./lua/keymaps.lua
+require("autocommands")  -- ./lua/autocommands.lua
 require("plugins-setup") -- ./lua/plugins-setup.lua
 vim.cmd("colorscheme gruvbox")
